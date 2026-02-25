e0be266feda2512fa9da87d12e224d4af19679f1
No-Jyun <hj010410@gmail.com>
2026-02-25 15:22:32 +0900

    창 크기 조정 및 생성 위치 가운데로 지정

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -16,7 +16,7 @@ int main()
     std::wstring className = L"Craft Engine Window";
     std::wstring title = L"Craft Engine";
     
-    // 창 크기
+    // 클라이언트 영역 창 크기
     uint32_t width = 1280;
     uint32_t height = 800;
 
@@ -35,6 +35,22 @@ int main()
         return 0;
     }
 
+    // 창 크기 구하기
+    RECT rect;
+    rect.left = 0;
+    rect.top = 0;
+    rect.right = width;
+    rect.bottom = height;
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    // 창 크기
+    uint32_t windowWidth = rect.right - rect.left;
+    uint32_t windowHeight = rect.bottom - rect.top;
+
+    // 창 생성 위치 가운데로 (좌상단의 위치를 구함)
+    uint32_t positionX = (GetSystemMetrics(SM_CXSCREEN) - windowWidth) / 2;
+    uint32_t positionY = (GetSystemMetrics(SM_CYSCREEN) - windowHeight) / 2;
+
     // 창 객체 생성
     HWND hwnd = CreateWindow(
         className.c_str(),       // Window class
@@ -42,7 +58,7 @@ int main()
         WS_OVERLAPPEDWINDOW,     // Window style
 
         // 위치 및 크기
-        CW_USEDEFAULT, CW_USEDEFAULT, width, height,
+        positionX, positionY, windowWidth, windowHeight,
 
         nullptr,       // Parent window    
         nullptr,       // Menu
@@ -56,6 +72,10 @@ int main()
         return 0;
     }
 
+    // 창의 클라이언트 크기 구하기
+    //RECT rect;
+    //GetClientRect(hwnd, &rect);
+    
     // 창 보이기 설정
     ShowWindow(hwnd, SW_SHOW);
 
